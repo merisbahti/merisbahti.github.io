5e949d46e1b7d547a99c089a3b9629931ad747d6
meris <meris.bahti@gmail.com>
2025-07-21 09:49:24 +0200

    start generating html

--- gen_html.clj
+++ gen_html.clj
@@ -1,7 +1,8 @@
 (ns gen-html
   (:require
    [hiccup.page :refer [html5]]
-   [hiccup2.core :as h]))
+   [hiccup2.core :as h]
+   [nextjournal.beholder :as beholder]))
 
 (def document
   (list
@@ -206,5 +207,29 @@ requestAnimationFrame(() => {
        {:href "https://lup.lub.lu.se/student-papers/search/publication/8887271"}
        "lup.lub.lu.se/student-papers/search/publication/8887271"]]]]))
 
+(def output-file "index.html")
+(defn generate-html []
+  (println "generating " output-file)
+  (spit output-file (str (html5 (h/html document)))))
+
+(defn watch []
+  (generate-html)
+  (let [watcher (atom nil)
+        watch-fn (fn [] (println "starting watcher")
+                   (reset! watcher
+                           (beholder/watch
+                            (fn [{path :path type :type}]
+                              (if
+                               (not (= (str path) output-file))
+                                (generate-html)
+                                (println "skipping update to " (str path))))
+                            "./")))]
+    (.start (Thread.
+             (fn []
+               (reset! watcher (watch-fn)))))
+    (Thread/sleep (* 24 60 60000))
+    (when @watcher (beholder/stop @watcher))
+    (println "thread dead")))
+
 (defn main []
-  (spit "myhtml.html" (str (html5 (h/html document)))))
+  (generate-html))
