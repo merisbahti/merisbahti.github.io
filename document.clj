cac671d1879ec62781abda2d3857127cf79420d2
meris <meris.bahti@gmail.com>
2025-07-21 15:10:16 +0200

    add title

--- document.clj
+++ document.clj
@@ -3,6 +3,7 @@
 (def document
   (list
    [:head
+    [:title "Meris Bahtijaragic - CV"]
     [:meta {:charset "UTF-8"}]
     [:meta {:name "viewport", :content "width=device-width, initial-scale=1.0"}]
     [:link
